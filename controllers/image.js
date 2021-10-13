62797700428581fba6a9e5520bffb3e05223fdc7
Jose Mario Fajardo <freemansfreedom@gmail.com>
2021-10-13 14:15:05 +0200

    Added Clarifai API key as an env var

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require ("clarifai");
 
 const app = new Clarifai.App({
-  apiKey: 'INSERT YOUR API KEY HERE'
+  apiKey: process.env.CLARIFAI_API_KEY
 });
 
 const apiCall = (req,res) => {
